7ecebac1b1c6c9c0ae6f1890a092c5a5a1b7c597
DaniOspina <danielaos94@gmail.com>
2024-12-11 11:03:15 -0500

    task

--- task1.js
+++ task1.js
@@ -1,20 +1,6 @@
-export function CostCalculator(transactionAmount) {
-    this.transactionAmount = transactionAmount;
-
-    this.payments = function () {
-        return this.transactionAmount + (transactionAmount*0.01) + 3
-    }
+export function costCalculator(num) {
+    let transactionCost = Number((num + 3 + (num * 0.01)));  
+    return transactionCost
 }
-
-const monthly = new CostCalculator(Number(process.argv[3]))
-// const monthly = new CostCalculator(124)
-
-console.log(monthly.payments());
-
-
-// export function costCalculator(num) {
-//     let transactionCost = Number((num + 3 + (num * 0.01)));  
-//     return transactionCost
-// }
-// const cost = costCalculator(Number(process.argv[3]))
-// console.log(cost)
+const cost = costCalculator(Number(process.argv[3]))
+console.log(cost)
